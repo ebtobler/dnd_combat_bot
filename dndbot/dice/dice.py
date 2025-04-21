0aaf8d7e982f66412b164314aa02956c8479ac98
ejt1037 <ejt1037@unh.edu>
2025-04-20 21:11:50 -0400

    removed comment

--- dndbot/dice/dice.py
+++ dndbot/dice/dice.py
@@ -137,36 +137,3 @@ class D1(Dice, ABC):
     @staticmethod
     def average_roll(n: int):
         return 1
-
-"""
-    @staticmethod
-    def d100(n: int):
-        return [randint(1, 100) for _ in range(n)]
-
-    @staticmethod
-    def d20(n: int):
-        return [randint(1, 20) for _ in range(n)]
-
-    @staticmethod
-    def d12(n: int):
-        return [randint(1, 12) for _ in range(n)]
-
-    @staticmethod
-    def d10(n: int):
-        return [randint(1, 10) for _ in range(n)]
-
-    @staticmethod
-    def d8(n: int):
-        return [randint(1, 18) for _ in range(n)]
-
-    @staticmethod
-    def d6(n: int):
-        return [randint(1, 6) for _ in range(n)]
-
-    @staticmethod
-    def d4(n: int):
-        return [randint(1, 4) for _ in range(n)]
-
-    @staticmethod
-    def d1(n: int):
-        return [1 for _ in range(n)]"""
